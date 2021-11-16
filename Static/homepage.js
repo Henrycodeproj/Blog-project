c53e3dbf6896ec103febc52eefdf146491420b20
Henry <lihenryhl@yahoo.com>
2021-11-15 20:00:31 -0800

    big changes for profile pics post pics editing post etc.

---
+++ Static/homepage.js
@@ -0,0 +1,10 @@
+// alert function to delete, will get back to later
+function myfunction() {
+    var txt;
+    if (confirm("Are you sure you want to delete this post?")) {
+      {{ redirect(url_for('post'))}}
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
